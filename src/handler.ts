63e4d707ba63734050f9bb61dcd49b0c7b43b36c
Xiphe <github@xiphe.net>
2022-01-25 19:48:52 +0100

    chore: source maps and debugging

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 import { S3Handler } from 'aws-lambda';
 
 const handler: S3Handler = async ({ Records }, context) => {
-  console.log({ Records });
+  console.log(Records[0]);
 };
 
 export default handler;
